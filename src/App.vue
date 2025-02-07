d684e3c7120b0e50bf2dc7b8f15c4ce178c96d30
Jayden Gillam <jaydster01@gmail.com>
2025-02-06 21:04:22 -0500

    second commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Order from './components/Order.vue'
+</script>
+
+<template>
+  <Order item1="Milk" item2="Eggs" item3="Bread" />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Order.vue
@@ -0,0 +1,67 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  item1: String,
+  item2: String,
+  item3: String,
+})
+
+const count1 = ref(0)
+const count2 = ref(0)
+const count3 = ref(0)
+
+</script>
+
+<template>
+  <h1>Order</h1>
+
+  <div class="cards">
+    <div class="card">
+      <h2>{{item1}}</h2>
+      <div class="counter">
+        <button type="button" @click="count1--">-</button>
+        <p>{{count1}}</p>
+        <button type="button" @click="count1++">+</button>
+      </div>
+    </div>
+    <div class="card">
+      <h2>{{item2}}</h2>
+      <div class="counter">
+        <button type="button" @click="count2--">-</button>
+        <p>{{count2}}</p>
+        <button type="button" @click="count2++">+</button>
+      </div>
+    </div>
+    <div class="card">
+      <h2>{{item3}}</h2>
+      <div class="counter">
+        <button type="button" @click="count3--">-</button>
+        <p>{{count3}}</p>
+        <button type="button" @click="count3++">+</button>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+.cards {
+  display: flex;
+  gap: 20px;
+  justify-content: center;
+  align-items: center;
+}
+.card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid #ccc;
+  padding: 10px;
+  border-radius: 5px;
+}
+.counter {
+  display: flex;
+  align-items: center;
+}
+</style>
